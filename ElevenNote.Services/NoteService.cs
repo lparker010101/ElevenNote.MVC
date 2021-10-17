3325ed2b86c2d085c7aaf7fdb03f6cf71834d3a9
Laura Parker <laura.parker03@yahoo.com>
2021-10-16 21:51:08 -0400

    NoteService Created

---
+++ ElevenNote.Services/NoteService.cs
@@ -0,0 +1,62 @@
+﻿using ElevenNote.Data;
+using ElevenNoteModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevenNote.Services
+{
+    public class NoteService
+    {
+        private readonly Guid _userId; // Created a constructor and a private field of type Guid. 
+
+        public NoteService(Guid userId) 
+        {
+            _userId = userId;
+        }
+
+        public bool CreateNote(NoteCreate model)
+        {
+            var entity =
+                new Note()
+                {
+                    OwnerId = _userId,
+                    Title = model.Title,
+                    Content = model.Content,
+                    CreatedUtc = DateTimeOffset.Now
+                };
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.Notes.Add(entity);
+                return ctx.SaveChanges() == 1;
+            }
+        }
+
+        public IEnumerable<NoteListItem> GetNotes()
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var query =
+                    ctx
+                        .Notes
+                        .Where(e => e.OwnerId == _userId)
+                        .Select(
+                            e =>
+                                new NoteListItem
+                                {
+                                    NoteId = e.NoteId,
+                                    Title = e.Title,
+                                    CreatedUtc = e.CreatedUtc
+                                }
+                        );
+                return query.ToArray();
+            }
+        }
+    }
+}
+
+// The service layer is how our application interacts with the database.  It will push and pull notes from the database.
+// We added references by right clicking on ElevenNote.Services to add a reference.  We selected Models and Data, so we 
+// can use the models and properties from those projects.  
